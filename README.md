3aca214620d7ec4f2f4c43e02bbb02c6ad5e65cc
shrunyan <shrunyan@gmail.com>
2021-09-15 11:33:14 -0700

    initial port into standalone repo

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # react-app-loader
-React component for loading a custom app via the app-sdk
+> React functional component which orchestrates the @zesty-io/app-sdk initialization
+
+Zesty.io custom apps are loaded by the [manager-ui](https://github.com/zesty-io/manager-ui) which passes the users session token to the custom application once it has completed loading in an iframe. This occurs asynchronously and therefore the mounting of the custom app, which depends on this token, must be delayed until ready.
+
+> NOTE: The component will display a loading screen while waiting for the session token.
+
+This component solves this for you and now loading your app becomes as simple as;
+
+```React
+import { AppLoader } from '@zesty-io/react-app-loader';
+import MyApp from "./path/to/MyApp";
+
+export default function App() {
+  return (
+      <AppLoader>
+        <MyApp />
+      </AppLoader>
+  );
+}
+```
---
+++ index.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+
+import SDK from "@zesty-io/app-sdk";
+
+const bar = {
+    animation: "ZestyAppLoaderFade 1s infinite ease-in-out",
+    backgroundColor: "#5b667d",
+    margin: "0.5rem",
+    opacity: "1",
+    width: "1rem"
+}
+
+export function AppLoader(props) {
+    const [token, setToken] = useState(props.token || "")
+    const [instance, setInstance] = useState(props.instance || {})
+
+    useEffect(() => {
+        // Pass token on init if provided, otherwise 
+        // sdk will listen for token from parent window
+        SDK.init(token, instance).then(json => {
+            setInstance(json.instance)
+            setToken(json.token)
+        });
+    }, [])
+
+    useEffect(() => {
+        var style = document.createElement("style");
+        style.textContent = `  @keyframes ZestyAppLoaderFade {
+            0%,
+            80%,
+            100% {
+              opacity: 1;
+            }
+            40% {
+              opacity: 0.3;
+            }
+          } `;
+        document.body.appendChild(style);
+
+        // TODO should we cleanup this style element when this component unmounts?
+    }, [])
+
+    return token && instance.ZUID ? (
+        React.cloneElement(props.children, {
+            SDK,
+            token,
+            instance
+        })
+    ) : (
+        <section
+            style={{
+                backgroundColor: "#f2f7fc",
+                height: props.height || "100%",
+                width: props.width || "100%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
+            <h3 style={{
+                color: "#5b667d",
+                fontSize: "32px",
+                fontFamily: "Mulish, Arial, Sans-Serif",
+                fontWeight: "300",
+                lineHeight: "48px",
+                letterSpacing: "0.32px"
+            }}>{props.message || "Starting Application"}</h3>
+            <div style={{
+                display: "flex",
+                height: "2rem"
+            }}>
+                <span style={{ ...bar, animationDelay: "-0.16s" }} />
+                <span style={{ ...bar, animationDelay: "1s" }} />
+                <span style={{ ...bar, animationDelay: "-0.32s" }} />
+                <span style={{ ...bar, animationDelay: "2s" }} />
+                <span style={{ ...bar, animationDelay: "-0.16s" }} />
+            </div>
+        </section>
+    );
+}
