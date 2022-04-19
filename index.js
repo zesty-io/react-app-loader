3ac59522cd4f3529a3f34339a45f87ee93cf8051
Ryan Pals <ryan.pals@zesty.io>
2022-04-18 23:49:22 -0500

    Revert "Manually transpile JSX code"
    This reverts commit 7115d46280cad51443b84d93c3526e5c7b09fb78.

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
 import SDK from "@zesty-io/app-sdk";
 
 const bar = {
-  animation: "ZestyAppLoaderFade 1s infinite ease-in-out",
-  backgroundColor: "#5b667d",
-  margin: "0.5rem",
-  opacity: "1",
-  width: "1rem",
-};
+    animation: "ZestyAppLoaderFade 1s infinite ease-in-out",
+    backgroundColor: "#5b667d",
+    margin: "0.5rem",
+    opacity: "1",
+    width: "1rem"
+}
 
 export function AppLoader(props) {
-  const [token, setToken] = useState(props.token || "");
-  const [instance, setInstance] = useState(props.instance || {});
+    const [token, setToken] = useState(props.token || "")
+    const [instance, setInstance] = useState(props.instance || {})
 
-  useEffect(() => {
-    // Pass token on init if provided, otherwise
-    // sdk will listen for token from parent window
-    SDK.init(token, instance).then((json) => {
-      setInstance(json.instance);
-      setToken(json.token);
-    });
-  }, []);
+    useEffect(() => {
+        // Pass token on init if provided, otherwise 
+        // sdk will listen for token from parent window
+        SDK.init(token, instance).then(json => {
+            setInstance(json.instance)
+            setToken(json.token)
+        });
+    }, [])
 
-  useEffect(() => {
-    var style = document.createElement("style");
-    style.textContent = `  @keyframes ZestyAppLoaderFade {
+    useEffect(() => {
+        var style = document.createElement("style");
+        style.textContent = `  @keyframes ZestyAppLoaderFade {
             0%,
             80%,
             100% {
@@ -35,68 +35,47 @@ export function AppLoader(props) {
               opacity: 0.3;
             }
           } `;
-    document.body.appendChild(style);
+        document.body.appendChild(style);
 
-    // TODO should we cleanup this style element when this component unmounts?
-  }, []);
+        // TODO should we cleanup this style element when this component unmounts?
+    }, [])
 
-  return token && instance.ZUID
-    ? React.cloneElement(props.children, {
-        SDK,
-        token,
-        instance,
-      })
-    : /*#__PURE__*/
-      React.createElement(
-        "section",
-        {
-          style: {
-            backgroundColor: "#f2f7fc",
-            height: props.height || "100%",
-            width: props.width || "100%",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-          },
-        },
-        /*#__PURE__*/ React.createElement(
-          "h3",
-          {
-            style: {
-              color: "#5b667d",
-              fontSize: "32px",
-              fontFamily: "Mulish, Arial, Sans-Serif",
-              fontWeight: "300",
-              lineHeight: "48px",
-              letterSpacing: "0.32px",
-            },
-          },
-          props.message || "Starting Application"
-        ),
-        /*#__PURE__*/ React.createElement(
-          "div",
-          {
-            style: {
-              display: "flex",
-              height: "2rem",
-            },
-          },
-          /*#__PURE__*/ React.createElement("span", {
-            style: { ...bar, animationDelay: "-0.16s" },
-          }),
-          /*#__PURE__*/ React.createElement("span", {
-            style: { ...bar, animationDelay: "1s" },
-          }),
-          /*#__PURE__*/ React.createElement("span", {
-            style: { ...bar, animationDelay: "-0.32s" },
-          }),
-          /*#__PURE__*/ React.createElement("span", {
-            style: { ...bar, animationDelay: "2s" },
-          }),
-          /*#__PURE__*/ React.createElement("span", {
-            style: { ...bar, animationDelay: "-0.16s" },
-          })
-        )
-      );
+    return token && instance.ZUID ? (
+        React.cloneElement(props.children, {
+            SDK,
+            token,
+            instance
+        })
+    ) : (
+        <section
+            style={{
+                backgroundColor: "#f2f7fc",
+                height: props.height || "100%",
+                width: props.width || "100%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
+            <h3 style={{
+                color: "#5b667d",
+                fontSize: "32px",
+                fontFamily: "Mulish, Arial, Sans-Serif",
+                fontWeight: "300",
+                lineHeight: "48px",
+                letterSpacing: "0.32px"
+            }}>{props.message || "Starting Application"}</h3>
+            <div style={{
+                display: "flex",
+                height: "2rem"
+            }}>
+                <span style={{ ...bar, animationDelay: "-0.16s" }} />
+                <span style={{ ...bar, animationDelay: "1s" }} />
+                <span style={{ ...bar, animationDelay: "-0.32s" }} />
+                <span style={{ ...bar, animationDelay: "2s" }} />
+                <span style={{ ...bar, animationDelay: "-0.16s" }} />
+            </div>
+        </section>
+    );
 }
